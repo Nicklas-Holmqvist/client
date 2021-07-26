61a51b784edc707428ef659350dbf5e05eb2126d
nicklas-holmqvist <nicklas_holmqvist@outlook.com>
2021-07-26 20:13:27 +0200

    now the fun begins

---
+++ src/components/header.tsx
@@ -0,0 +1,24 @@
+import React from "react";
+
+function Header() {
+
+    return(
+        <div className="header-section">
+            <div className="menu-btn">
+                H
+            </div>
+            <div className="logo">
+                <h3>Glömd värld</h3>
+            </div>
+            <div className="social-icons">
+                <ul>
+                    <li>F</li>
+                    <li>I</li>
+                    <li>M</li>
+                </ul>
+            </div>
+        </div>
+    )
+}
+
+export default Header;
---
+++ src/components/layout.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+import Footer from "./footer";
+import Header from "./header";
+
+function Layout() {
+
+    return (
+        <>
+            <Header />
+            <h1>Layout?</h1>
+            <Footer />
+        </>
+    );
+}
+
+export default Layout;
